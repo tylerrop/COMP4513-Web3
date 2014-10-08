c5fc9f268b8a388e1ac8d523bd91b8c2e5edba52
Tyler Rop <rop.tyler@gmail.com>
2014-10-08 00:18:32 -0600

    Completed Assignement 1. This took less than 2 weeks, was a ton of work, learned how to use PHP for the first time really.

---
+++ A1/footer.php
@@ -0,0 +1,11 @@
+
+
+<!-- <div class="footer">
+    <div class="container">
+    	<p class="text-muted">Place sticky footer content here.</p>
+    </div>
+</div> -->
+
+</body>
+ 
+</html>
---
+++ A1/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html> 
+<html>
+<head lang="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Dynamically get page title -->
+    <title><?php echo $title; ?></title>
+
+    <link rel="icon" href="favicon.ico">  
+    
+    <!-- Bootrap CSS -->
+    <link href="bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/dashboard.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+
+    <!-- Custom styles for overriding classes and defaults -->
+    <link href="css/override.css" rel="stylesheet">
+
+</head>
+
+<body>
