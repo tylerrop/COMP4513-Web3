8e3af5554eff440a1463c14cde4439432be3ec6d
Tyler Rop <rop.tyler@gmail.com>
2014-10-20 14:52:38 -0600

    Mving JS back into the Head

--- A2/footer.php
+++ A2/footer.php
@@ -10,19 +10,7 @@
 
 
 
-<!-- jQuery -->
-<script src="//code.jquery.com/jquery-1.11.1.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js" type="text/javascript"></script>
 
-<!-- Slippry Slider -->
-<script src="/js/slippry.min.js"></script>
-<script type="text/JavaScript" src="/js/slippryInitialization.js"></script>
-    
-<!-- Custom jQuery -->
-<script type="text/JavaScript" src="/js/formContext.js"></script>
-
-<!-- Table sorter -->
-<script src="/sort-animated/tsort.min.js"></script>
 
 <!-- initiate table sorter for serach results -->
 <script type="text/javascript">
--- A2/header.php
+++ A2/header.php
@@ -33,6 +33,21 @@
     
     <link rel="stylesheet" href="/sort-animated/style.css" type="text/css" />
 
+
+    <!-- jQuery -->
+    <script src="//code.jquery.com/jquery-1.11.1.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js" type="text/javascript"></script>
+
+    <!-- Slippry Slider -->
+    <script src="/js/slippry.min.js"></script>
+    <script type="text/JavaScript" src="/js/slippryInitialization.js"></script>
+    
+    <!-- Custom jQuery -->
+    <script type="text/JavaScript" src="/js/formContext.js"></script>
+
+    <!-- Table sorter -->
+    <script src="/sort-animated/tsort.min.js"></script>
+
 </head>
 
 <body>
